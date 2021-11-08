713d322f0e245b52e29e65a220b6a317bd3ef7ba
Eduardo <eduardoldq1@gmail.com>
2021-11-08 11:31:54 -0300

    feat: sql file with database implementation

--- init.sql
+++ init.sql
@@ -1,28 +1,28 @@
-CREATE DATABASE plantonet;
+CREATE DATABASE plantonetgo;
 
-use plantonet;
+\connect plantonetgo
 
-CREATE TABLE gerente(
-   nome VARCHAR(50) NOT NULL,
-   telefone VARCHAR(15) NOT NULL,
+CREATE TABLE manager(
+   name VARCHAR(50) NOT NULL,
+   cell_number VARCHAR(15) NOT NULL,
    cpf VARCHAR(11) PRIMARY KEY NOT NULL,
    email VARCHAR(30) NOT NULL,
-   senha VARCHAR(30) NOT NULL
+   password VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE medico(
-   nome VARCHAR(50) NOT NULL,
-   telefone VARCHAR(15) NOT NULL,
+CREATE TABLE doctor(
+   name VARCHAR(50) NOT NULL,
+   cell_number VARCHAR(15) NOT NULL,
    cpf VARCHAR(11) PRIMARY KEY NOT NULL,
    email VARCHAR(30) NOT NULL,
-   senha VARCHAR(30) NOT NULL,
-   especialidade VARCHAR(30) NOT NULL,
-   crm VARCHAR(15) NOT NULL,
+   password VARCHAR(30) NOT NULL,
+   specialty VARCHAR(30) NOT NULL,
+   crm VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE plantao(
-   data_plantao DATE,
-   tipo VARCHAR(20),
-   cpf_medico VARCHAR(11),
-   CONSTRAINT cpf_medico_fk FOREIGN KEY (cpf_medico) REFERENCES medico(cpf),
+CREATE TABLE medical_duty(
+   date_duty DATE,
+   type VARCHAR(20),
+   cpf_doctor VARCHAR(11),
+   CONSTRAINT cpf_doctor_fk FOREIGN KEY (cpf_doctor) REFERENCES doctor(cpf)
 );
